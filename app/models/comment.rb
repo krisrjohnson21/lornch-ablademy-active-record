3eafffcfe14e85a91f0ce35a7e414b7fcebbd6af
Kris Johnson <krisrjohnson@gmail.com>
2020-01-16 16:29:40 -0500

    updated model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
 
   validates :asker, presence: true
   validates :body, presence: true
+  validates :clinic_id, presence: true
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,7 @@
 class Signup < ActiveRecord::Base
   belongs_to :student
   belongs_to :clinic
+
+  validates :student_id, presence: true
+  validates :clinic_id, presence: true
 end
